0a31f78df1d7ca2fe197ff156746d5bbbaa1a33e
Darya Jenkins <melissegreenleaf@gmail.com>
2024-12-23 12:25:42 +0300

    BeforeEach added

--- src/test/java/ru/netology/repository/ShopRepositoryTest.java
+++ src/test/java/ru/netology/repository/ShopRepositoryTest.java
@@ -1,6 +1,7 @@
 package ru.netology.repository;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Product;
 
@@ -10,11 +11,14 @@ public class ShopRepositoryTest {
     Product product1 = new Product(13, "Кошачий корм", 900);
     Product product2 = new Product(14, "Мячик для кошки", 80);
 
-    @Test
-    public void shouldRemoveByIdSuccessfully() {
+    @BeforeEach
+    public void setup() {
         repository.add(product1);
         repository.add(product2);
+    }
 
+    @Test
+    public void shouldRemoveByIdSuccessfully() {
         repository.removeById(product1.getId());
 
         Product[] expected = {product2};
@@ -23,9 +27,6 @@ public class ShopRepositoryTest {
 
     @Test
     public void shouldRemoveUnsuccessfully() {
-        repository.add(product1);
-        repository.add(product2);
-
         Assertions.assertThrows(NotFoundException.class, () -> {
             repository.removeById(15);
         });
@@ -33,18 +34,12 @@ public class ShopRepositoryTest {
 
     @Test
     public void shouldAddToArraySuccessfully() {
-        repository.add(product1);
-        repository.add(product2);
-
         Product[] expected = {product1, product2};
         Assertions.assertArrayEquals(expected, repository.findAll());
     }
 
     @Test
     public void shouldAddUnsuccessfully() {
-        repository.add(product1);
-        repository.add(product2);
-
         Assertions.assertThrows(AlreadyExistsException.class, () -> {
             repository.add(product1);
         });
